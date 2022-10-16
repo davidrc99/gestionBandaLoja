c4449531379ed19d1e5cf4ca4626510b45365445
David Roman Castellano <droman@omegacrmconsulting.com>
2022-10-16 20:02:26 +0200

    buscar archivos pdf DONE

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from mimetypes import init
 from pathlib import *
 from config import *
 import os
+from enum import Enum
 
 #Escogido el tipo de partitura, saber en qué directorios buscar
 def searchDirectory(tipo):
@@ -10,22 +11,18 @@ def searchDirectory(tipo):
         list_dir.append(Path(DIR_MARCHAS))
     elif tipo == 'pasodoble' or tipo == 'pasodobles':
         list_dir.append(Path(DIR_PASODOBLES))
-    elif tipo == 'obra' or tipo == 'obras':
-        list_dir.append(Path(DIR_OBRAS))
-    elif tipo == 'otra' or tipo == 'otras':
-        list_dir.append(Path(DIR_OTRAS))
-    else:
-        list_dir.append(Path(DIR_MARCHAS))
-        list_dir.append(Path(DIR_PASODOBLES))
-        list_dir.append(Path(DIR_OBRAS))
-        list_dir.append(Path(DIR_OTRAS))
+    elif tipo == 'obra' or tipo == 'obras':             
+        list_dir.append(Path(DIR_OBRAS))             
+    elif tipo == 'otra' or tipo == 'otras':         
+        list_dir.append(Path(DIR_OTRAS))             
+    else:             
+        print('ERROR - NO EXISTE ESE TIPO DE PARTITURA')         
     return list_dir
 
 #Dado el tipo de partitura y su nombre, buscar su ruta en los directorios
 def searchSheet(list_dir, nombre):
     #Si solo se busca en un directorio
     rutaPartitura = ''
-    print(list_dir)
     if(len(list_dir) == 1):
         directorio = list_dir[0]
         directorio : Path
@@ -36,14 +33,22 @@ def searchSheet(list_dir, nombre):
                 break
             else:
                 print('ERROR - NO SE ENCONTRÓ LA PARTITURA')
-                rutaPartitura = 'ERROR'
-    else:
-        for dir in list_dir:
-            list_dir_aux = []
-            list_dir_aux.append(dir)
-            searchSheet(dir,nombre)
+                break
 
-    return  rutaPartitura    
+    return  rutaPartitura
+
+def getVoicesSheets(directory):   
+    voicesSheet = []
+    for x in directory.iterdir():
+        if x.is_file():
+            partitura = dict(
+                nombre= x.stem,
+                path = str(x)
+            )
+            voicesSheet.append(partitura)
+
+    return voicesSheet
+
 
 #funcion para convertir el nombre a "nombreMarchaNummeroDos"
 def toCamelCase(name):
@@ -56,36 +61,18 @@ def toCamelCase(name):
 #funcion para buscar el nombre dentro de la carpeta
 
 
-
-
-def searching_all_files(directory: Path):   
-    file_list = [] # A list for storing files existing in directories
-
-    for x in directory.iterdir():
-        if x.is_file():
-           file_list.append(x)
-        else:
-
-           file_list.append(searching_all_files(directory/x))
-
-    return file_list
-
-
-p = Path(DIR_RAIZ)
-
-# for file in Path(DIR_RAIZ):
-#     if file.is_file():
-
-
 if __name__ == "__main__":
     print('Introduzca el tipo de partitura:')
     tipo = input()
-    print('Introduzca un nomnbre de marcha:')
-    nombre = input()
-
     list_dir = searchDirectory(toCamelCase(tipo))
-
-    print(searchSheet(list_dir,toCamelCase(nombre)))
-
-
+    if(len(list_dir) == 1):
+        print('Introduzca un nomnbre de marcha:')
+        nombre = input()
+        rutaPartitura = searchSheet(list_dir,toCamelCase(nombre))
+        if(rutaPartitura):
+            print('Introduzca la voz que desa buscar:')
+            listaVoces = getVoicesSheets(rutaPartitura)
+            for voz in listaVoces:
+                print(voz['nombre'])
+            
 
